d12d00e8a67a8ae0feab3e7b78af3d5adc9565dd
Sarvesh Bajaj <bt22cse087@iiitn.ac.in>
2024-10-02 20:18:49 +0530

    fixed paths

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import About from '../components/About'
-import EventPage from '../components/EventPage/EventPage'
+import EventPage from '../components/Eventpage/EventPage'
 import NewComp from './../components/NewComp';
 // import Hero2 from './../components/Hero2';
 
