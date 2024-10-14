2d91d4b8828f482db860e47cffe4d0529e38ea40
Saksham Agrawal <agsaksham07@gmail.com>
2024-10-14 11:10:00 +0530

    added imgs

--- src/components/EventDetails/Event.jsx
+++ src/components/EventDetails/Event.jsx
@@ -40,7 +40,7 @@ const ClubEvents = ({ name, description, image, events, id }) => {
 
     useEffect(() => {
 
-        setWidth(document.querySelector(".card").offsetWidth + 100);
+        setWidth(document.querySelector(".card").offsetWidth + 96);
 
         // setInterval(() => {
         //     buttonRight?.current.click()
@@ -66,45 +66,40 @@ const ClubEvents = ({ name, description, image, events, id }) => {
             {/* <div className="h-[15vh] w-full bg-green-200">Logo</div> */}
             <div className=" lg:h-[45vh] md:h-[30vh] h-[30vh] w-full flex justify-between mt-14">
                 <div className="h-full w-full items-start justify-between px-10 flex text-white">
-                    <div className="h-fit w-[40%] flex flex-col-reverse lg:flex-row items-center justify-center md:justify-between">
+                    <div className="h-fit w-[40%] flex flex-col-reverse lg:flex-row items-center justify-center md:justify-start">
                         <div className="h-fit flex items-center justify-between">
-                            <div className="h-20 md:h-36 lg:h-64 aspect-square rounded-full img mr-10" style={{ backgroundImage: `url(${image})` }} />
+                            <div className="h-20 md:h-36 lg:h-64 aspect-square rounded-full img mr-10 bg-no-repeat" style={{ backgroundImage: `url(${image})` }} />
                         </div>
                         <div>
-                            <p className="flex flex-col text-3xl md:text-7xl uppercase break-words">{name}</p>
+                            <p className="flex flex-col frontman leading-10 text-xl md:text-4xl uppercase break-words md:mr-5">{name}</p>
                             <p className="flex flex-col text-xs md:text-sm mt-4 md:my-8 break-words text-[#717A80]">{description}</p>
                         </div>
                     </div>
                     <div>
                         <p className="text-3xl md:text-5xl flex items-center">
-                            <span>03</span>
-                            <span>
-                                <Slash className="mt-2" size={35} color="#717A80" />
-                            </span>
-                            <span>08</span>
+                            Contact :
                         </p>
                         <div className="text-xs md:text-base">
                             <div className="mt-10">
-                                <p>Role</p>
+                                <p>Club Lead</p>
                                 <p className="-mt-1 md:-mt-2 text-[#717A80]">Full Stack Dev / Motion</p>
                             </div>
                             <div>
-                                <p>Agency</p>
+                                <p>Club Co-Lead</p>
                                 <p className="-mt-1 md:-mt-2 text-[#717A80]">Things</p>
                             </div>
                             <div>
-                                <p>Year</p>
+                                <p>Phone</p>
                                 <p className="-mt-1 md:-mt-2 text-[#717A80]">2023</p>
-                            </div>
-                            <div>
-                                <p>Awards</p>
-                                <p className="-mt-1 md:-mt-2 text-[#717A80]">CSSDA of the Day</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="h-[80vh] w-full flex items-center">
+                {/* {events.length > 2 && <button className="h-[50%] w-10 lg:mx-5 flex justify-center items-center nextBtn hover:animate-pulse" onClick={() => handleScroll(-width)}>
+                    <ChevronsLeft />
+                </button>} */}
                 <button className="h-[50%] w-10 lg:mx-5 flex justify-center items-center nextBtn hover:animate-pulse" onClick={() => handleScroll(-width)}>
                     <ChevronsLeft />
                 </button>
@@ -115,6 +110,9 @@ const ClubEvents = ({ name, description, image, events, id }) => {
                         );
                     })}
                 </div>
+                {/* {events.length > 2 && <button className="h-[50%] w-10 lg:mx-5 flex justify-center items-center nextBtn hover:animate-pulse" onClick={() => handleScroll(width)} ref={buttonRight}>
+                    <ChevronsRight />
+                </button>} */}
                 <button className="h-[50%] w-10 lg:mx-5 flex justify-center items-center nextBtn hover:animate-pulse" onClick={() => handleScroll(width)} ref={buttonRight}>
                     <ChevronsRight />
                 </button>
@@ -135,7 +133,7 @@ const EventCard = ({ image, name, description, prize, date, link }) => {
 
     return (
         <div
-            className="relative h-[75vh] md:h-[90vh] lg:h-[100%] w-[100%] md:w-[10rem] lg:w-[44%] flex flex-none flex-col glass-morph card lg:px-0 clip-path bg-gradient-to-br from-[#aa14f0] via-[#aa14f02e] to-[#b936f500] overflow-hidden"
+            className="relative h-[60vh] md:h-[90vh] lg:h-[100%] w-[100%] md:w-[10rem] lg:w-[44%] flex flex-none flex-col glass-morph card lg:px-0 clip-path overflow-hidden border-none"
             style={{
                 "--x": `${coords.x}px`,
                 "--y": `${coords.y}px`,
@@ -143,11 +141,11 @@ const EventCard = ({ image, name, description, prize, date, link }) => {
             onMouseMove={handleMouseMove}
         >
             <div className="absolute inset-0 z-10 before:absolute before:content-[''] before:top-[var(--y)] before:left-[var(--x)] before:transform before:-translate-x-1/2 before:-translate-y-1/2 before:bg-[radial-gradient(circle,var(--clr,rgba(255,255,255,0.2)),transparent,transparent)] before:w-[700px] before:h-[700px] before:opacity-0 hover:before:opacity-100 before:transition-opacity before:duration-500"></div>
-            <div className="h-[40%] lg:h-[750px] w-full lg:w-full bg-cover bg-top bg-no-repeat z-0 event-img" style={{ backgroundImage: `url(${image})` }}></div>
+            <div className="h-[40%] lg:h-[700px] w-full lg:w-full bg-cover bg-top bg-no-repeat z-0" style={{ backgroundImage: `url(${image})` }}></div>
 
-            <div className="h-[800px] w-full lg:max-h-60 flex flex-col lg:flex-row justify-between items-start px-5 pb-10 tracking-wide z-9">
+            <div className="h-[500px] w-full lg:max-h-60 flex flex-col lg:flex-row justify-between items-start px-2 lg:px-5 pb-2 lg:pb-10 pt-2 mt-2 tracking-wide z-9">
                 <div className="w-full lg:w-1/2">
-                    <p className="text-lg lg:text-xl font-extrabold break-words uppercase tracking-wide mb-5 orbitron">{name}</p>
+                    <p className="text-lg lg:text-xl font-extrabold break-words uppercase tracking-wide mb-0 orbitron">{name}</p>
                     <p className="text-xs font-normal">{description}</p>
                 </div>
                 <div className="h-full flex flex-col justify-between items-start lg:items-center">
--- src/components/EventDetails/data.js
+++ src/components/EventDetails/data.js
@@ -1,13 +1,14 @@
 import { images } from "./EventImages/Images"; 
+import { clubImages } from "./ClubLogos/images"; 
 
 const data = [
     {
         name: "DOTSLASH",
         description: "Comepetetive Programming Club of IIIT Nagpur",
-        image: "/3.jpg",
+        image: clubImages.dotslash,
         events: [
             {
-                image: images[1],
+                image: images.quantQuest,
                 name: "QUANT QUEST",
                 description:
                     "Quant Quest is an algorithmic trading competition hosted by IIIT Nagpur. Participants, either solo or in teams of up to three, design trading strategies to maximize Gross ROI. The strategies are tested in a simulated market environment reflecting real-world scenarios, with a focus on Indian or US markets.",
@@ -17,7 +18,7 @@ const data = [
                 
             },
             {
-                image: images[1],
+                image: images.algorithmia,
                 name: "Algorithimia",
                 description:
                     "Algorithmia is an ICPC-style competitive programming contest by IIIT Nagpur, featuring two rounds: an online round on Codeforces, and an offline round at IIITN campus. Teams of 1-3 solve 6-10 problems across varying difficulty levels.",
@@ -26,7 +27,7 @@ const data = [
                 link: "https://unstop.com/p/algorithmia-tantrafiesta24-iiit-nagpur-1152434",
             },
             // {
-            //     image: images[1],
+            //     image: images.royalShowdown,
             //     name: "Codefiesta",
             //     description:
             //         "NA",
@@ -39,10 +40,10 @@ const data = [
     {
         name: "CRISPR",
         description: "",
-        image: "/3.jpg",
+        image: clubImages.crispr,
         events: [
             {
-                image: images[1],
+                image: images.analytica,
                 name: "Analytica",
                 description:
                 "Analytica is a machine learning and data science hackathon, with Round 1 on Unstop and Round 2 offline on Kaggle. It encourages creative problem-solving using data science and machine learning techniques.",
@@ -52,7 +53,7 @@ const data = [
                 
             },
             {
-                image: images[1],
+                image: images.royalShowdown,
                 name: "GPT 4o mini Solve-A-Thon",
                 description:
                     "Gen-AI Hackathon is an online and offline event where participants create innovative solutions to problem statements, focusing on code functionality, efficiency, creativity, and AI model use (e.g., GPT-4).",
@@ -65,10 +66,10 @@ const data = [
     {
         name: "IOTICS",
         description: "Internet of Things and Robotics Club of IIIT Nagpur",
-        image: "/3.jpg",
+        image: clubImages.iotics,
         events: [
             {
-                image: images[1],
+                image: images.roboRumble,
                 name: "Robo Rumble",
                 description:
                     "The Robo Rumble event challenges teams from different colleges to design and build their own RC cars according to our specifications. The event is designed to test the participant’s engineering skills, creativity, and strategy through a series of competitive rounds.",
@@ -78,7 +79,7 @@ const data = [
                 
             },
             {
-                image: images[1],
+                image: images.royalShowdown,
                 name: "Trail Tracer: Line Following Bot",
                 description:
                     "Trail Tracer is a competition where participants build autonomous robots to follow a dark path on a white background, navigating twists and turns while avoiding dead ends to reach the endpoint with precision and speed.",
@@ -87,7 +88,7 @@ const data = [
                 link: "https://unstop.com/p/trail-tracer-line-following-bot-tantrafiesta24-iiit-nagpur-1152285",
             },
             {
-                image: images[1],
+                image: images.royalShowdown,
                 name: "ByteCraft : AIoT challenge",
                 description:
                     "This competition invites teams to explore the potential of AIoT in healthcare, developing a prototype that solves real-world problems, optimizes operations, and creates intelligent environments to improve quality of life.",
@@ -100,10 +101,10 @@ const data = [
     {
         name: "ELEVATE",
         description: "",
-        image: "/3.jpg",
+        image: clubImages.elevate,
         events: [
             {
-                image: images[1],
+                image: images.genethon,
                 name: "Genathon 2.0",
                 description:
                     "Genathon 2.0 is a hackathon where participants develop innovative solutions using any tech stack, including Web Development or AI/ML. Multiple judging rounds will select the top three teams with the most effective solutions.",
@@ -113,7 +114,7 @@ const data = [
                 
             },
             {
-                image: images[1],
+                image: images.royalShowdown,
                 name: "Bug Bounty Blitz",
                 description:
                     "Bug Bounty Blitz is a quest where participants find, report, and optionally solve bugs in designated repositories. It emphasizes technical skill and creativity, with bugs of varying difficulty.",
@@ -122,7 +123,7 @@ const data = [
                 link: "https://unstop.com/p/bug-bounty-blitz-tantrafiesta24-iiit-nagpur-1152367",
             },
             {
-                image: images[1],
+                image: images.lastManStanding,
                 name: "Last Man Standing",
                 description:
                     "Last Man Standing is a competitive quiz challenge where teams test their knowledge, teamwork, and strategy, with a final round determining the last participant standing and the winning team.",
@@ -135,10 +136,10 @@ const data = [
     {
         name: "DIMENSIONS",
         description: "",
-        image: "/3.jpg",
+        image: clubImages.dimensions,
         events: [
             {
-                image: images[1],
+                image: images.royalShowdown,
                 name: "GAME JAM: COSMIC CRAFTATHON",
                 description:
                     "Participants in this game jam are tasked with developing a 2D space exploration game. The theme encourages creativity in designing gameplay mechanics that allow players to navigate space.",
@@ -148,7 +149,7 @@ const data = [
                 
             },
             {
-                image: images[1],
+                image: images.royalShowdown,
                 name: "RENDER RIOT",
                 description:
                     "We're excited to announce our 3D animation competition with the theme 'From Stone to Silicon.' This theme challenges participants to visually depict the evolution of technology—from the earliest stone tools to the advanced digital age we live in today.",
@@ -161,11 +162,11 @@ const data = [
     {
         name: "PROBE",
         description: "",
-        image: "/3.jpg",
+        image: clubImages.probe,
         events: [
             {
-                image: images[1],
-                name: "Digital Time capsule : Tantrafiesta memories",
+                image: images.digitalTimeCapsule,
+                name: "Digital Time capsule",
                 description:
                     "The Digital Time Capsule is an initiative capturing the essence of TantraFiesta, where participants submit creative content showcasing their memorable festival moments. This content will be digitally preserved.",
                 prize: "20000",
@@ -174,7 +175,7 @@ const data = [
                 
             },
             {
-                image: images[1],
+                image: images.brandxperience,
                 name: "BrandXperience",
                 description:
                     "We're excited to announce our 3D animation competition with the theme 'From Stone to Silicon.' This theme challenges participants to visually depict the evolution of technology—from the earliest stone tools to the advanced digital age we live in today.",
@@ -187,10 +188,10 @@ const data = [
     {
         name: "STROKES",
         description: "",
-        image: "/3.jpg",
+        image: clubImages.strokes,
         events: [
             {
-                image: images[1],
+                image: images.royalShowdown,
                 name: "Design-a-thon",
                 description:
                     "The design competition has two rounds: an online case study with design improvements, and a 12-hour offline round at IIITN, where participants tackle real-world design challenges with surprise twists.",
@@ -204,10 +205,10 @@ const data = [
     {
         name: "ORATOR",
         description: "",
-        image: "/3.jpg",
+        image: clubImages.orator,
         events: [
             {
-                image: images[1],
+                image: images.royalShowdown,
                 name: "Cypher Chronicles",
                 description:
                     "Become an investigative reporter who will explore, digest, and discover new, undiscovered facts for your readers. Like seasoned journalists, explore mysteries that lie deep inside, and unravel the unknown!",
@@ -219,12 +220,12 @@ const data = [
         ],
     },
     {
-        name: "TF CENTRAL TEAM",
+        name: "TF TEAM",
         description: "Tantra Fiesta - The Tech Fest of IIIT Nagpur",
-        image: "/3.jpg",
+        image: clubImages.tf,
         events: [
             {
-                image: images[1],
+                image: images.royalShowdown,
                 name: "EnigmaXplore 2.0",
                 description:
                     "EngimaXplore will be a cyber security competition- Jeopardy Style,  for participants to show their skills in various areas of computing and solve real-life security challenges.",
@@ -234,7 +235,7 @@ const data = [
                 
             },
             {
-                image: images[1],
+                image: images.royalShowdown,
                 name: "Women Only Hackthon",
                 description:
                     "WomenInDev 1.0 is a women-only hackathon where participants solve real-world problems using technologies like web development, AI/ML, and data science. The top teams are judged on creativity, effectiveness, and real-world impact.",
@@ -243,7 +244,7 @@ const data = [
                 link: "https://unstop.com/p/womenindev-womens-only-hackathon-tantrafiesta24-iiit-nagpur-1153208",
             },
             {
-                image: images[1],
+                image: images.royalShowdown,
                 name: "FreeFire",
                 description:
                     "The Free Fire Competition requires all teams to thoroughly review and follow the tournament rules and regulations. By participating, teams agree to abide by these rules, and any violations will result in immediate disqualification without further discussion.",
@@ -252,7 +253,7 @@ const data = [
                 link: "https://unstop.com/p/battle-of-fire-freefire-tantrafiesta24-iiit-nagpur-1151619",
             },
             {
-                image: images[1],
+                image: images.royalShowdown,
                 name: "CODM",
                 description:
                     "Call of Duty comepetition",
@@ -261,7 +262,7 @@ const data = [
                 link: "https://unstop.com/p/warzone-warrior-codm-tantrafiesta24-iiit-nagpur-1151609",
             },
             {
-                image: images[1],
+                image: images.royalShowdown,
                 name: "Clash Royale",
                 description:
                     "Clash Royale comepetition",
@@ -270,7 +271,7 @@ const data = [
                 link: "https://unstop.com/p/royal-showdown-clash-royale-tantrafiesta24-iiit-nagpur-1151587",
             },
             {
-                image: images[1],
+                image: images.chessShatranjUp,
                 name: "Chess",
                 description:
                     "The Checkmate Challenge is a two-day chess competition held as part of Tantrafiesta 2k24, offering participants the chance to showcase their chess skills.",
@@ -283,10 +284,10 @@ const data = [
     {
         name: "UDYAM",
         description: "",
-        image: "/3.jpg",
+        image: clubImages.udyam,
         events: [
             {
-                image: images[1],
+                image: images.royalShowdown,
                 name: "Innovation Oasis",
                 description:
                     "Innovation Oasis 2024 celebrates innovation and entrepreneurship, where participants pitch startup ideas to judges, focusing on creativity and business viability. It's aimed at students passionate about turning their ideas into reality.",
@@ -296,7 +297,7 @@ const data = [
                 
             },
             {
-                image: images[1],
+                image: images.vyaparVichar,
                 name: "Vyapar Vichar",
                 description:
                     "Vyapaar Vichar is an event aimed at showcasing the marketing talents of aspiring entrepreneurs, encouraging participants to think creatively, strategize effectively, and present their ideas with confidence in the dynamic entrepreneurial landscape.",
