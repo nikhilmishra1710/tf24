32c8b40838bd09e4bfbda968dbde955de2f7fba6
Sarvesh Bajaj <bt22cse087@iiitn.ac.in>
2024-10-15 18:05:34 +0530

    fixed issues

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
+import { motion } from 'framer-motion';
 import "./Team.css";
 
-const Team = ({title,members}) => {
+const Team = ({ title, members }) => {
 
     const [mousePos, setMousePos] = useState({ x: '50%', y: '50%' });
 
-  const handleMouseMove = (e) => {
-    const rect = e.target.getBoundingClientRect();
-    const x = ((e.clientX - rect.left) / rect.width) * 100;
-    const y = ((e.clientY - rect.top) / rect.height) * 100;
-    setMousePos({ x: `${x}%`, y: `${y}%` });
-  };
+    const handleMouseMove = (e) => {
+        const rect = e.target.getBoundingClientRect();
+        const x = ((e.clientX - rect.left) / rect.width) * 100;
+        const y = ((e.clientY - rect.top) / rect.height) * 100;
+        setMousePos({ x: `${x}%`, y: `${y}%` });
+    };
+
+    const cardVariants = {
+        hidden: { opacity: 0, y: 50 },
+        visible: { opacity: 1, y: 0, transition: { duration: 0.3, ease: 'easeInOut' } },
+    };
 
     return (
         <div className="team-section">
@@ -18,12 +24,24 @@ const Team = ({title,members}) => {
             <div className="line"></div>
             <div className="team-list">
                 {members.map((member) => (
-                    <div className="team-member" key={member.Name} onMouseMove={handleMouseMove} style={{ '--mouseX': mousePos.x, '--mouseY': mousePos.y }}>
+
+                    <motion.div
+                        className="team-member"
+                        key={member.Name}
+                        onMouseMove={handleMouseMove}
+                        style={{
+                            '--mouseX': mousePos.x, '--mouseY': mousePos.y
+                        }}
+                        initial="hidden"
+                        whileInView="visible"
+                        variants={cardVariants}
+                        viewport={{ once: true, amount: 0.3 }}
+                    >
                         <img src={"/eventcards/crispr.png"} alt={member.Name} className="member-image" />
                         <h4>{member.Name}</h4>
                         <p>{member.designation}</p>
                         <p>Phone: {member.Phone}</p>
-                    </div>
+                    </motion.div>
                 ))}
             </div>
         </div>
