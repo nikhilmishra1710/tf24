57643f69f1785bdb6c75b29bccaf65e5807ebe97
nikhilmishra17 <nikhilmishra1710@gmail.com>
2024-10-15 12:14:27 +0530

    file updates

--- src/components/EventDetails/EventImages/Images.js
+++ src/components/EventDetails/EventImages/Images.js
@@ -1,51 +1,27 @@
-import algorithmia from "./algorithmia.svg"
-import quantQuest from "./quant_quest.svg"
-import analytica from "./analytica.svg"
-import gpt from "./gpt.svg"
-import genethon from "./genethon.svg"
-import bugBountyBlitz from "./bug_bounty_blitz.svg"
-import lastManStanding from "./last_man_standing.svg"
-import cosmicCraftathon from "./cosmic_craftathon.svg"
-import renderRiot from "./render_riot.svg"
-import designathon from "./designathon.svg"
-import cypherChronicles from "./cypher_chronicles.svg"
-import enigmaXplore from "./enigma_xplore.svg"
-import womenInDev from "./women_in_dev.svg"
-import roboRumble from "./robo_rumble.svg"
-import byteCraft from "./byte_craft.svg"
-import trailTracer from "./trail_tracer.svg"
-import brandxperience from "./brandXperience.svg"
-import digitalTimeCapsule from "./digital_time_capsule.svg"
-import royalShowdown from "./royal_showdown.svg"
-import chessShatranjUp from "./chess_shatranj_up.svg"
-import freeFire from "./free_fire.svg"
-import codm from "./warzone_warrior_codm.svg"
-import vyaparVichar from "./vyapar_vichar.svg"
-import innovationOasis from "./innovation_oasis.svg"
-
 export const images = {
-    algorithmia: algorithmia,
-    quantQuest: quantQuest,
-    analytica: analytica,
-    gpt: gpt,
-    genethon: genethon,
-    bugBountyBlitz: bugBountyBlitz,
-    lastManStanding: lastManStanding,
-    cosmicCraftathon: cosmicCraftathon,
-    renderRiot: renderRiot,
-    designathon: designathon,
-    cypherChronicles: cypherChronicles,
-    enigmaXplore: enigmaXplore,
-    womenInDev: womenInDev,
-    roboRumble: roboRumble,
-    byteCraft: byteCraft,
-    trailTracer: trailTracer,
-    digitalTimeCapsule: digitalTimeCapsule,
-    brandxperience:brandxperience,
-    royalShowdown:royalShowdown,
-    chessShatranjUp: chessShatranjUp,
-    freeFire: freeFire,
-    codm: codm,
-    vyaparVichar: vyaparVichar,
-    innovationOasis: innovationOasis
-};
+    algorithmia: "images/EventImages/algorithmia.png",
+    quantQuest: "images/EventImages/quantQuest.png",
+    analytica: "images/EventImages/analytica.png",
+    gpt: "images/EventImages/gpt.png",
+    genethon: "images/EventImages/genethon.png",
+    bugBountyBlitz: "images/EventImages/bugBountyBlitz.png",
+    lastManStanding: "images/EventImages/lastManStanding.png",
+    cosmicCraftathon: "images/EventImages/cosmicCraftathon.png",
+    renderRiot: "images/EventImages/renderRiot.png",
+    designathon: "images/EventImages/designathon.png",
+    cypherChronicles: "images/EventImages/cypherChronicles.png",
+    enigmaXplore: "images/EventImages/enigmaXplore.png",
+    womenInDev: "images/EventImages/womenInDev.png",
+    roboRumble: "images/EventImages/roboRumble.png",
+    byteCraft: "images/EventImages/byteCraft.png",
+    trailTracer: "images/EventImages/trailTracer.png",
+    digitalTimeCapsule: "images/EventImages/digitalTimeCapsule.png",
+    brandxperience: "images/EventImages/brandXperience.png",
+    royalShowdown: "images/EventImages/royalShowdown.png",
+    chessShatranjUp: "images/EventImages/chessShatranjUp.png",
+    freeFire: "images/EventImages/freeFire.png",
+    codm: "images/EventImages/codm.png",
+    vyaparVichar: "images/EventImages/vyaparVichar.png",
+    innovationOasis: "images/EventImages/innovationOasis.png",
+  };
+  
