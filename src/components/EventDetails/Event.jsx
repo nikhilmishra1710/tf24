183a0fc5c18c63d618d441166b7b089082f36412
Saksham Agrawal <agsaksham07@gmail.com>
2024-10-15 18:54:01 +0530

    data updated

--- src/components/EventDetails/Event.jsx
+++ src/components/EventDetails/Event.jsx
@@ -25,7 +25,7 @@ function Event() {
             </div>
             {
                 data.map((club, index) => {
-                    return <ClubEvents name={club.name} description={club.description} image={club.image} events={club.events} key={index} id={club.name.toLowerCase()} />;
+                    return <ClubEvents name={club.name} description={club.description} image={club.image} events={club.events} key={index} id={club.name.toLowerCase()} lead={club.lead} coLead={club.coLead} phone={club.phone} />;
                 })
             }
         </>
@@ -35,7 +35,7 @@ function Event() {
 export default Event;
 
 
-const ClubEvents = ({ name, description, image, events, id }) => {
+const ClubEvents = ({ name, description, image, events, id, lead, coLead, phone }) => {
     const [scrollPosition, setScrollPosition] = useState(0);
     const [width, setWidth] = useState(0);
     const [arrows, setArrows] = useState(true);
@@ -80,7 +80,7 @@ const ClubEvents = ({ name, description, image, events, id }) => {
     };
 
     return (
-        <div className="w-[100vw] lg:h-[135vh] md:h-[100vh] h-[120vh] flex flex-col px-0 md:px-16 text-white" id={id}>
+        <div className="w-[100vw] lg:h-[135vh] md:h-[100vh] h-[120vh] flex flex-col px-0 md:px-16 text-white orbitron" id={id}>
             {/* <div className="h-[15vh] w-full bg-green-200">Logo</div> */}
             <div className=" lg:h-[35vh] md:h-[30vh] h-[30vh] w-full flex justify-between mt-14">
                 <div className="h-full w-full items-start justify-between px-10 flex text-white">
@@ -89,26 +89,26 @@ const ClubEvents = ({ name, description, image, events, id }) => {
                             <div className="h-20 md:h-36 lg:h-44 aspect-square rounded-full img mr-10 bg-no-repeat" style={{ backgroundImage: `url(${image})` }} />
                         </div>
                         <div>
-                            <p className="flex flex-col frontman leading-10 text-xl md:text-4xl uppercase break-words md:mr-5">{name}</p>
+                            <p className="flex flex-col frontman leading-10 tracking-wider text-lg md:text-4xl uppercase break-words md:mr-5 font-light">{name}</p>
                             <p className="flex flex-col text-xs mb-3 md:text-sm mt-0.5 md:my-4 break-words text-[#717A80]">{description}</p>
                         </div>
                     </div>
-                    <div className="lg:mr-16">
+                    <div className="lg:mr-16 font-light tracking-wider">
                         <p className="text-3xl md:text-5xl flex items-center">
                             Contact
                         </p>
                         <div className="text-xs md:text-base">
                             <div className="mt-2">
-                                <p>Club Lead</p>
-                                <p className="-mt-1 md:-mt-2 text-[#717A80]">Full Stack Dev / Motion</p>
+                                <p>Lead</p>
+                                <p className="-mt-1 md:-mt-2 text-[#717A80]">{lead}</p>
                             </div>
-                            <div>
-                                <p>Club Co-Lead</p>
-                                <p className="-mt-1 md:-mt-2 text-[#717A80]">Things</p>
-                            </div>
+                            {coLead&&<div>
+                                <p>Co-Lead</p>
+                                <p className="-mt-1 md:-mt-2 text-[#717A80]">{coLead}</p>
+                            </div>}
                             <div>
                                 <p>Phone</p>
-                                <p className="-mt-1 md:-mt-2 text-[#717A80]">2023</p>
+                                <p className="-mt-1 md:-mt-2 text-[#717A80]">{phone}</p>
                             </div>
                         </div>
                     </div>
@@ -145,7 +145,7 @@ const EventCard = ({ image, name, description, prize, date, link }) => {
 
     return (
         <div
-            className="relative h-[60vh] md:h-[90vh] lg:h-[100%] w-[100%] md:w-[10rem] lg:w-[44%] flex flex-none flex-col items-center glass-morph card lg:px-0 clip-path overflow-hidden border-none"
+            className="relative h-[62vh] md:h-[90vh] lg:h-[100%] w-[100%] md:w-[10rem] lg:w-[44%] flex flex-none flex-col items-center glass-morph card lg:px-0 clip-path overflow-hidden border-none"
             style={{
                 "--x": `${coords.x}px`,
                 "--y": `${coords.y}px`,
@@ -161,12 +161,12 @@ const EventCard = ({ image, name, description, prize, date, link }) => {
                 </div>
                 <div className="h-full flex flex-col justify-between items-start lg:items-center">
                     <div className="my-4 lg:my-0 text-left lg:text-right">
-                        <p className="text-xl font-bold">{`Prize Pool : ₹ ${prize}`}</p>
-                        <p className="font-bold">{`Date : ${date}`}</p>
+                        <p className="text-lg font-bold">{`Prize Pool : ₹ ${prize}`}</p>
+                        <p className="text-sm font-bold">{`Date : ${date}`}</p>
                     </div>
-                    <div className="mb-5">
+                    {link&&<div className="mb-5">
                         <RegisterButton link={link} />
-                    </div>
+                    </div>}
                 </div>
             </div>
         </div>
