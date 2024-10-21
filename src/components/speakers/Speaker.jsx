e31a5cb3e0c4a1d2168e3dfc3e19fb474f39f1c4
Saksham Agrawal <agsaksham07@gmail.com>
2024-10-21 23:50:31 +0530

    sanjay arora image updated

--- src/components/speakers/Speaker.jsx
+++ src/components/speakers/Speaker.jsx
@@ -10,7 +10,7 @@ const Speaker = () => {
             <div className='speaker-title bstyle-bold uppercase'>Speakers</div>
             <div className='speakers'>
                 {/* <Card3 img={"speakercards/Kanwar_Singh.jpeg"}  name={'CFO & Vice President of Finance at Oracle India Pvt. Ltd'} number={'Mr. Kanwar Singh'} boxType1={'A'} boxType2={'B'}/> */}
-                <Card3 img={"speakercards/Sanjay_Arora.jpeg"}  name={'Founder & CEO at Shells Advertising '} number={' Dr. Sanjay Arora'}/>
+                <Card3 img={"speakercards/Sanjay_Arora.png"}  name={'Founder & CEO at Shells Advertising '} number={' Dr. Sanjay Arora'}/>
                 <Card3 img={"speakercards/Gajendra_purohit.jpeg"}  name={'Founder and CEO of Mathscare'} number={'Dr. Gajendra Purohit'}/>
                 <Card3 img={"speakercards/Roshni_Chhabra.png"}  name={' Senior Success Manager at Sprinklr'} number={'Ms. Roshni Chhabra'} />
                 {/* <Card3 img={"speakercards/Salin_Agrawal.jpeg"}  name={'CA, Finance Strategy maker at CRED'} number={' Mr. Salin Agarwal'}/> */}
